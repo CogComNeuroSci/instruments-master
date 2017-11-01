a02c96ecb56659b213e8d9440230e5a133343dfa
Esther De Loof <estherdeloof@gmail.com>
2017-11-01 14:02:45 +0100

    update comments

--- Scripts/Lesson_5_data_types.py
+++ Scripts/Lesson_5_data_types.py
@@ -110,11 +110,10 @@ print (c)
 
 # Python interprets / on integers as long division (e.g., 7/2 = 3).
 # If you don't like it, you can import a different definition of division as follows:
-# just make sure to put the import statement at the top of the file
-# then it will work
-from __future__ import division
-print(7/2) # becomes standard division
-print(7//2.0) # remains long division
+# (see the additional script named Lesson_5_data_types_division for an example)
+# from __future__ import division
+# print(7/2) # becomes standard division
+# print(7//2.0) # remains long division
 
 ####################
 ## Exponentiation ##
