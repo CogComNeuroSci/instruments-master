71f23646493e6fd926b3c30b0b9f5254c13b6e18
Esther De Loof <estherdeloof@gmail.com>
2017-11-01 22:11:02 +0100

    remove useless packages

--- Scripts/Lesson_5_printing.py
+++ Scripts/Lesson_5_printing.py
@@ -3,10 +3,8 @@
 ############
 
 from __future__ import division
-from psychopy import core, visual, event
+from psychopy import core
 import time
-import platform
-import ctypes
 
 
 ###################
