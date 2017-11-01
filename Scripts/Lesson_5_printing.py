626c50e434fcc0510182122b4d0c1e1b6d8dc2fb
Esther De Loof <estherdeloof@gmail.com>
2017-11-01 22:09:38 +0100

    unify quotes

--- Scripts/Lesson_5_printing.py
+++ Scripts/Lesson_5_printing.py
@@ -5,13 +5,8 @@
 from __future__ import division
 from psychopy import core, visual, event
 import time
-
 import platform
 import ctypes
-
-print('Hi there')
-print('This is the second line')
-print('Bye')
 
 
 ###################
@@ -44,12 +39,12 @@ print(number)
 ## "The number is 10"
 ## Try replacing the 10 with another numerical value to see what will happen!
 
-print('The number is {0}'.format(number))
+print("The number is {0}".format(number))
 
 # The zero between {} indicates means that this is the first number (Python starts counting at zero).
 # Actually, because there is just one number, the explicit rank order (0) is required and we can write
 
-print('The number is {}'.format(number))
+print("The number is {}".format(number))
 
 # The format string allows a great deal of flexibility for writing to the screen. Here is another example:
 number1=1
@@ -67,25 +62,25 @@ print("percentage is {:%}".format(number1/number2))
 
 ## Here, we let PsychoPy wait for 1 second to execute the following command
 ## Between the brackets, we define how many seconds PsychoPy waits to execute the rest of the code
-print('Wait a second...')
+print("Wait a second...")
 time.sleep(1)
 
 ## Here, we let PsychoPy wait for 2 second to execute the following command
-print('Or maybe two...')
+print("Or maybe two...")
 time.sleep(2)
 
-print('Done!')
+print("Done!")
 
 ## Again, we let PsychoPy wait for 1 second to execute the following command
 ## The commands time.sleep() and core.wait() are in essence the same, as they both halt the execution of a program
-print('Wait a second...')
+print("Wait a second...")
 core.wait(1)
 
 ## Again, the execution is halted for 2 seconds 
-print('Or maybe two...')
+print("Or maybe two...")
 core.wait(2)
 
-print('Done!')
+print("Done!")
 
 #########
 ## END ##
