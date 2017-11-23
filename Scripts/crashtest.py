bdbda4b28e3bcb2e8789ba9f11a30d293542eb72
Tom Verguts <tom.verguts@ugent.be>
2017-11-23 09:49:38 +0100

    add crash test

---
+++ Scripts/crashtest.py
@@ -0,0 +1,7 @@
+# Code for crashing cars
+# by Esther De Loof & Tom Verguts, nov 2017
+# part 1: initialisation
+from psychopy import visual
+from time import sleep
+# part 2: driving around
+# part 3: print results on screen
