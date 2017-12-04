187716ae4bf691d637f37d227e8f0ab91179fd03
Tom Verguts <tom.verguts@ugent.be>
2017-12-04 11:08:19 +0100

    change to english

--- Scripts/Lesson_8_RT_measurement_dialog.py
+++ Scripts/Lesson_8_RT_measurement_dialog.py
@@ -7,14 +7,14 @@ import time
 
 # initialize stuff
 win = visual.Window(size=[500,400])
-tekst1 = visual.TextStim(win,text="are you ready...? ")
-tekst2 = visual.TextStim(win,text="Go!")
-tekst_juist=visual.TextStim(win,text="correct :-)")
-tekst_fout=visual.TextStim(win,text="wrong :-(")
-klok=core.Clock()
+text1 = visual.TextStim(win,text="are you ready...? ")
+text2 = visual.TextStim(win,text="Go!")
+text_correct=visual.TextStim(win,text="correct :-)")
+text_wrong=visual.TextStim(win,text="wrong :-(")
+clock=core.Clock()
 info={"name": "name", "subject nr": 1}
 my_dialog=gui.DlgFromDict(dictionary=info,title="Give your data")
-tijd=[] # empty list
+times=[] # empty list
 n_trial=3
 key_list=["f","j"]
 correct_list=["f","f","j"]
@@ -24,27 +24,27 @@ print(info)
 # start the process
 for loop in range(n_trial):
     n = random.randint(1,3) # wait a sec (or two)
-    tekst1.draw()
+    text1.draw()
     win.flip()
     time.sleep(n)
-    tekst2.draw()
+    text2.draw()
     win.flip()
-    klok.reset() 
+    clock.reset() 
     keys=event.waitKeys(key_list)
-    tijd.append(klok.getTime())
+    times.append(clock.getTime())
     if "escape" in keys:
         core.quit()
     if keys[-1] in correct_list[loop]:
-        tekst_juist.draw()
+        text_correct.draw()
     else:
-        tekst_fout.draw()
+        text_wrong.draw()
     win.flip()
     time.sleep(1)
 
 # calculate stats
-meantime=np.mean(tijd)
-tekst3 = visual.TextStim(win,text="mean RT = {0:.1f} sec".format(meantime),pos=[0,0.5])
-tekst3.draw()
+meantime=np.mean(times)
+text3 = visual.TextStim(win,text="mean RT = {0:.1f} sec".format(meantime),pos=[0,0.5])
+text3.draw()
 win.flip()
 while not event.getKeys():
     pass
