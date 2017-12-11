b19cad7a63c002d0d687ef821a9590baff8c2950
Esther De Loof <estherdeloof@gmail.com>
2017-12-11 20:12:57 +0100

    unify code in 8.1

--- Scripts/CE 8.1.py
+++ Scripts/CE 8.1.py
@@ -14,13 +14,15 @@ win = visual.Window(size=[500,400])
 
 # initializing
 my_clock = core.Clock()
-time_list = [] ## empty list
+RT = [] ## empty list
 n_trials = 3
 key_list = ["f","j"]
 correct_list = ["f","f","j"]
-key_qwerty=["a","z"] # incomplete list!
-key_azerty=["q","w"] # incomplete list!
-key_board_azerty = False # switch the lists above if you use AZERTY
+
+## This code can be used if you have trouble with an azerty keyboard.
+##key_qwerty=["a","z"] # incomplete list!
+##key_azerty=["q","w"] # incomplete list!
+##key_board_azerty = False # switch the lists above if you use AZERTY
 
 # graphical elements
 text_ready = visual.TextStim(win,text="are you ready...?")
@@ -44,17 +46,18 @@ for loop in range(n_trials):
     ## reset the clock to measure the RT
     my_clock.reset() 
 
-    ## wait for the key press and register it
-#    keys=[""]
-#    while not keys[0] in key_list:
-#        keys=event.waitKeys()
-#        if key_board_azerty==True and keys[0] in key_azerty:
-#            keys[0] = key_qwerty[key_azerty.index(keys[0])]
+## This code can be used if you have trouble with an azerty keyboard.
+##    keys=[""]
+##    while not keys[0] in key_list:
+##        keys=event.waitKeys()
+##        if key_board_azerty==True and keys[0] in key_azerty:
+##            keys[0] = key_qwerty[key_azerty.index(keys[0])]
 
+    ## wait for the key press and register it
     keys = event.waitKeys(keyList = ["f","j"])
 
     ## register the time
-    time_list.append(my_clock.getTime())
+    RT.append(my_clock.getTime())
 
     ## display the accuracy feedback (predetermined)
     if keys[-1] in correct_list[loop]:
@@ -65,11 +68,11 @@ for loop in range(n_trials):
     time.sleep(1)
 
 # display the average RT for one second
-meantime = np.mean(time_list)
+meantime = np.mean(RT)
 text_feedback = visual.TextStim(win,text="mean RT = {0:.1f} sec".format(meantime),pos=[0,0.5])
 text_feedback.draw()
 win.flip()
-time.sleep(1)
+time.sleep(3)
 
 # wrap it up
 win.close()
