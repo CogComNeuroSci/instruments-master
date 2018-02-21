c8635b60139f8dfe27fd7fa16ed6bca43a96a52b
Esther De Loof <estherdeloof@gmail.com>
2018-02-21 11:21:08 +0100

    add some breathing space

--- Scripts/Lesson_12_ExperimentHandler.py
+++ Scripts/Lesson_12_ExperimentHandler.py
@@ -4,12 +4,12 @@
 # attempts to write anyway in case of crash
 # written by Tom Verguts, Feb 2018
 
-from psychopy import data, visual, event, core
+from psychopy import visual, data, event, core
 
 visual_targets = [digit for digit in range(1,8)]
 targets_responses = []
-data_file="experimental_data.csv"
-thisExp = data.ExperimentHandler(dataFileName=data_file)
+data_file = "experimental_data.csv"
+thisExp = data.ExperimentHandler(dataFileName = data_file)
 
 for loop in visual_targets:
     if loop%2 == 0:
@@ -19,7 +19,7 @@ for loop in visual_targets:
     targets_responses.append({'target':loop, 'correct_response':correct_response})
 
 # shakearound(targets_responses) # Here you can add code from Lesson 11 (randomization) to shake around the list according to one's experimental desires
-trials = data.TrialHandler(targets_responses, nReps=1, method='random')
+trials = data.TrialHandler(targets_responses, nReps = 1, method = 'random')
 win = visual.Window([400,400])
 
 experiment_timer = core.Clock()
@@ -28,24 +28,24 @@ thisExp.addLoop(trials)
 
 for trial in trials: # a TrialHandler object is iterable
     experiment_timer.reset()
-    the_text = visual.TextStim(win, text=trial['target'], color='white')
+    the_text = visual.TextStim(win, text = trial['target'], color = 'white')
     the_text.draw()
     win.flip()
     trial_continue = True
     while trial_continue:
-        response=event.getKeys()
+        response = event.getKeys()
         if response:
             trial_continue = False
     rt = experiment_timer.getTime()
     accuracy = 0
-    if response[0]==trial['correct_response']:
+    if response[0] == trial['correct_response']:
         accuracy = 1
     trials.addData('response', response[0])
     trials.addData('accuracy', accuracy)
-    trials.addData('RT',rt)
+    trials.addData('RT', rt)
     thisExp.nextEntry()
 
-thisExp.saveAsWideText(data_file, appendFile=False)
+thisExp.saveAsWideText(data_file, appendFile = False)
 thisExp.abort()
 
 win.close()
