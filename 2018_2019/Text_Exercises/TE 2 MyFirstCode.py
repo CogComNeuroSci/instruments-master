011a3891f25b7454f298bbb5352641ddda9fd167
Esther De Loof <estherdeloof@gmail.com>
2018-10-02 11:47:52 +0200

    add solutions to the text exercises for own use chapter 2

---
+++ 2018_2019/Text_Exercises/TE 2 MyFirstCode.py
@@ -0,0 +1,4 @@
+# This is the first code I ever wrote in Python.
+# Did so on October 24, 2018, and it was a great experience.
+print(3+3)
+print("Hello world!")
---
+++ 2018_2019/Text_Exercises/TE 2 MyFourthCode.py
@@ -0,0 +1,17 @@
+# goal of this code: make a window, draw a window, and close it finally.
+
+from psychopy import visual
+# visual is a module from psychopy containing several visual methods
+
+import time 
+# time is a module from python containing several time-related methods
+# Hence, the window will be shown for 3 seconds exactly
+
+win = visual.Window([600,400])
+
+grating = visual.GratingStim(win, mask = "circle", ori = 45)
+grating.draw()
+
+win.flip()
+time.sleep(3)
+win.close()
