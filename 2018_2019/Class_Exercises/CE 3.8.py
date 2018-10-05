52bce1c5af612c19d10c58ca2ec788a745ad29c0
Esther De Loof <estherdeloof@gmail.com>
2018-10-05 23:50:05 +0200

    add exercises to TE and CE for stroop task chapter 3

--- 2018_2019/Class_Exercises/CE 3.8.py
+++ 2018_2019/Class_Exercises/CE 3.8.py
@@ -5,34 +5,28 @@
 # Max score on each course is 20.
 
 # import modules
-from __future__ import division
-from psychopy import visual, event
+from psychopy import visual
+import time, numpy
 
 # initializing
 name        = "jasper francken"
 student_nr  = 2
-scores      = [12, 13, 9, 18]
+scores      = numpy.array([12, 13, 9, 18])
 directory   = "/Users/tom/Documents/student_data/"  ## you'll want to insert a path for your own directory
+
+# total of correct items
+tot_correct = sum(scores>9)
+l           = len(scores)
 
 # initialize the window
 win = visual.Window([800,400])
 
-# here starts a (not necessarily the best) solution
-# note about the next code line: cleaner code is possible if one uses a for-loop or numpy,
-# but neither have been discussed yet in class
-# Remember, True + True = 2
-tot_correct = (scores[0]>9)+(scores[1]>9)+(scores[2]>9)+(scores[3]>9)
-l = len(scores)
-
 # note about the next code line: It's also possible to do this with one long string and several arguments in the format() argument
-text1 = ("Good morning " + name.split()[0].capitalize() +"!\n\nWe now have the scores of all "+
-        str(l)+" tests that you did. You passed {:.0%}".format(tot_correct/l)+" of your courses."+
-        "Your data will be written to "+directory+"student{:03d}".format(student_nr)+".txt")
-t1 = visual.TextStim(win,text=text1,wrapWidth=1.1)
-t1.draw()
+messageText = ( "Good morning " + name.split()[0].capitalize() + "!\n\nWe now have the scores of all " +
+                str(l) + " tests that you did. You passed {:.0%}".format(tot_correct/l) + " of your courses." +
+                "Your data will be written to " + directory + "student{:03d}".format(student_nr) + ".txt")
+message = visual.TextStim(win, text = messageText, wrapWidth = 1.1)
+message.draw()
 win.flip()
-
-# next line keeps the screen active until the f key is pressed
-while not "f" in event.getKeys():
-    pass
+time.sleep(2)
 win.close()
---
+++ 2018_2019/Text_Exercises/TE 3.22.py
@@ -0,0 +1,9 @@
+import numpy
+
+my_array1 = numpy.array([[1, 2, 3], [4, 5, 6]])
+my_array2 = my_array1
+
+my_array2[0,1] = 99
+
+print(my_array2)
+print(my_array1)
