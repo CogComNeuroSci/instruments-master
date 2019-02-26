5bfad215b5c65ebf56c9dd9d8cc826746fc8b97b
Esther De Loof <estherdeloof@gmail.com>
2019-02-26 15:48:18 +0100

    add files for CE 8 FAQ on tiling versus concatenating

---
+++ 2018_2019/Class_Exercises/CE 8.1 FAQ Tile.py
@@ -0,0 +1,73 @@
+# Code for Stroop task randomization
+# Blocked randomization
+
+import pandas
+import numpy
+
+
+# constants
+
+## number of blocks
+nBlocks = 10
+
+## number of trials per block
+nBlockTrials = 80
+
+
+# make the design based on the core trial characteristics
+
+## declare all levels of the factor
+ColorOptions = numpy.array(["red","blue","green","yellow"])
+
+## determine the number of levels for the factor
+Ncolors = len(ColorOptions)
+Nunique = Ncolors * Ncolors
+
+## determine the number of unique trials
+UniqueTrials = numpy.array(range(Nunique)) 
+
+## make the 4-by-4 factorial design
+ColorWord = numpy.floor(UniqueTrials / Ncolors)
+FontColor = numpy.floor(UniqueTrials / 1) %  Ncolors
+
+## combine arrays in trial matrix
+Design = numpy.column_stack([ColorWord, FontColor])
+
+
+# make the design for one block
+
+## number of design repetitions per block
+nReps = int(nBlockTrials/Nunique)
+
+## repeat the 4-by-4 design five times
+blockTrials = numpy.tile(Design, (nReps, 1))
+
+
+# fill in the random trial order per block
+
+## randomize the trial order
+numpy.random.shuffle(blockTrials)
+
+## make trial matrix
+trials = numpy.tile(blockTrials, (nBlocks, 1))
+
+## fill in the block number (starting from 1 instead of 0)
+blocknr = numpy.repeat(numpy.array(range(nBlocks)), blockTrials.shape[0])
+
+## add the block numbers to the trial matrix
+trials = numpy.column_stack([trials,blocknr])
+
+
+# Validation and export
+
+## creating pandas dataframe from numpy array
+trials = pandas.DataFrame.from_records(trials)
+
+## name the columns
+trials.columns = ["ColorWord", "FontColor", "Block"]
+
+## cross table validation
+print(pandas.crosstab([trials.ColorWord, trials.FontColor], trials.Block))
+
+## export
+trials.to_csv(path_or_buf = "CE8_1_output_arrays_tilestructure.csv", index = False)
