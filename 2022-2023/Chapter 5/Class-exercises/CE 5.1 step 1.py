acd1c96bb2147ea64b8a88c7cd4f7f26d662b24f
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2022-11-28 15:17:13 +0100

    Add files via upload

---
+++ 2022-2023/Chapter 5/Class-exercises/CE 5.1 step 1.py
@@ -0,0 +1,158 @@
+# displaying Stroop stimuli
+
+# import modules
+from psychopy import visual, event
+import time, numpy
+
+# initialize the window
+win = visual.Window(fullscr = True, units = "norm")
+
+# initialize the variables
+nblocks     = 2
+ntrials     = 16
+participant = 2
+
+# we start with adding the values for the words and the colors
+ColorWord   = numpy.array([ "red", "red", "red", "red",
+                            "blue", "blue", "blue", "blue",
+                            "green", "green", "green", "green",
+                            "yellow", "yellow", "yellow", "yellow"])
+FontColor   = numpy.array([ "red", "blue", "green", "yellow",
+                            "red", "blue", "green", "yellow",
+                            "red", "blue", "green", "yellow",
+                            "red", "blue", "green", "yellow"])
+
+# deduce the congruence
+CongruenceLevels    = numpy.array(["Incongruent", "Congruent"])
+CongruenceBoolean   = numpy.array(ColorWord == FontColor)
+Congruence          = CongruenceLevels[[CongruenceBoolean*1]]
+
+# deduce the task instruction
+if participant%2 == 0:
+    # participants with an even number have to respond to the color word
+    CorResp = numpy.copy(ColorWord)
+else:
+    # participants with an odd number have to respond to the ink color
+    CorResp = numpy.copy(FontColor)
+
+# deduce the correct response
+CorResp[CorResp == "red"]     = "d"
+CorResp[CorResp == "blue"]    = "f"
+CorResp[CorResp == "green"]   = "j"
+CorResp[CorResp == "yellow"]  = "k"
+
+# allow to store the accuracy
+Accuracy = numpy.repeat(-99.9,len(CorResp))
+
+# combine arrays in trial matrix
+trials = numpy.column_stack([ColorWord, FontColor, Congruence, CorResp, Accuracy])
+
+# repeat the trial matrix for the two blocks
+trials = numpy.tile(trials, (nblocks, 1))
+
+# initialize graphical elements
+Welcome         = visual.TextStim(win, text = "Welcome!\n\nPress the space bar to continue.")
+Instructions    = visual.TextStim(win, text = "OK", height = 0.05)
+Block_start     = visual.TextStim(win, text = "OK")
+Stroop_stim     = visual.TextStim(win, text = "red", color = "blue")
+Feedback        = visual.TextStim(win, text = "OK")
+Goodbye         = visual.TextStim(win, text = "Goodbye!", pos = (0,0.75), height = 0.2)
+TheEndImage     = visual.ImageStim(win, image = "the_end.jpg")
+
+# deduce the task instruction
+if participant%2 == 0:
+    # participants with an even number have to respond to the color word
+    Instructions.text = (   "In this experiment you will see color words (“red”, “blue”, “green” and “yellow”)\n" +
+                            "presented in a random ink color (red, blue, green and yellow color).\n\n" +
+                            "You have to respond to the meaning of the written word and\n" +
+                            "ignore the ink color of the stimulus.\n\n" +
+                            "You can use the following four response buttons (from left to right;\n" +
+                            "use the index and middle finger of your left and right hand):" +
+                            "“d”,“f”,“j” and “k”.\n\n" +
+                            "If the written word is red, press the leftmost button “d”.\n" +
+                            "If it’s blue, press “f”.\n" +
+                            "If it’s green, press “j”.\n" +
+                            "If it’s yellow, press “k”.\n\n" +
+                            "Answer as quickly as possible, but also try to avoid mistakes.\n" +
+                            "By all means ignore the ink color, you should only respond to the meaning of the words.\n\n" +
+                            "Any questions?\n\nPress the space bar to continue.")
+else:
+    # participants with an odd number have to respond to the ink color
+    Instructions.text = (   "In this experiment you will see color words (“red”, “blue”, “green” and “yellow”)\n" +
+                            "presented in a random ink color (red, blue, green and yellow color).\n\n" +
+                            "You have to respond to the ink color of the stimulus and\n" +
+                            "ignore the meaning of the written word.\n\n" +
+                            "You can use the following four response buttons (from left to right;\n" +
+                            "use the index and middle finger of your left and right hand):" +
+                            "“d”,“f”,“j” and “k”.\n\n" +
+                            "If the ink color is red, press the leftmost button “d”.\n" +
+                            "If it’s blue, press “f”.\n" +
+                            "If it’s green, press “j”.\n" +
+                            "If it’s yellow, press “k”.\n\n" +
+                            "Answer as quickly as possible, but also try to avoid mistakes.\n" +
+                            "By all means ignore the meaning of the words, you should only respond to the ink color.\n\n" +
+                            "Any questions?\n\nPress the space bar to continue.")
+
+# display the welcome message
+Welcome.draw()
+win.flip()
+event.waitKeys(keyList = "space")
+
+# display the instructions
+Instructions.draw()
+win.flip()
+event.waitKeys(keyList = "space")
+
+# display the Stroop stimuli
+# in two blocks
+for b in range(nblocks):
+    
+    # announce what block is about to start
+    Block_start.text = "Block " + str(b+1) + " will start when you press the space bar."
+    Block_start.draw()
+    win.flip()
+    event.waitKeys(keyList = "space")
+    
+    # in 16 trials
+    for i in range(b*ntrials,(b+1)*ntrials):
+        
+        # set the color word and the font color for this trial
+        Stroop_stim.text    = trials[i,0]
+        Stroop_stim.color   = trials[i,1]
+        
+        # draw the stimulus on the back buffer
+        Stroop_stim.draw()
+        
+        # clear the keyboard input
+        event.clearEvents(eventType = "keyboard")
+        
+        # display the stimulus on the screen
+        win.flip()
+        
+        # Wait for the response
+        keys = event.waitKeys(keyList = ["d","f","j","k"])
+        
+        # determine accuracy
+        trials[i,4] = int(trials[i,3] == keys[0])
+        
+        # determine the feedback message
+        if int(trials[i,4]) == 1:
+            Feedback.text = "Correct!"
+        else:
+            Feedback.text = "Wrong answer!"
+        
+        # display the feedback message
+        Feedback.draw()
+        win.flip()
+        time.sleep(0.25)
+
+# display the goodbye message
+TheEndImage.draw()
+Goodbye.draw()
+win.flip()
+time.sleep(1)
+
+# close the experiment window
+win.close()
+
+print(trials)
---
+++ 2022-2023/Chapter 5/Class-exercises/CE 5.6.py
@@ -0,0 +1,101 @@
+"""
+some I/O testing in a basic "experiment"
+
+"""
+
+# import modules
+from psychopy import visual, event, core
+import time, numpy
+
+# initialize the window
+win = visual.Window(size=[500,400])
+
+# initializing
+my_clock = core.Clock()
+my_mouse = event.Mouse()
+n_trials = 3
+key_list = ["f","j"]
+
+# hard coded stimulus onset delay and correct response
+OnsetDelay  = numpy.array([1,0.5,2])
+CorResp     = numpy.array(["left","left","right"]) ## right-click means double-click on a Mac
+
+# add a default RT that will be overwritten during the trial loop
+RT = numpy.repeat(-99.9,n_trials)
+
+# graphical elements
+text_ready      = visual.TextStim(win,text="are you ready...?")
+text_go         = visual.TextStim(win,text="Go!")
+text_correct    = visual.TextStim(win,text="correct :-)")
+text_error      = visual.TextStim(win,text="wrong :-(")
+
+# perform three trials
+for trial in range(n_trials):
+
+    ## display the first message and wait a second (or two)
+    text_ready.draw()
+    win.flip()
+    time.sleep(OnsetDelay[trial])
+
+    ## draw the Go message on the back buffer
+    text_go.draw()
+    
+    ## clear the mouse input
+    event.clearEvents(eventType = "mouse")
+    
+    ## display the Go message on the screen
+    win.flip()
+
+    ## reset the clock to measure the RT
+    my_clock.reset() 
+
+    ## wait for the mouse press and register it
+    while numpy.sum(my_mouse.getPressed())==0:
+        pass
+
+    ## register the time
+    RT[trial] = my_clock.getTime()
+
+    ## display the accuracy feedback (predetermined)
+    if (my_mouse.getPressed()[0]==1 and CorResp[trial]=="left") or (my_mouse.getPressed()[2]==1 and CorResp[trial]=="right"):
+        text_correct.draw()
+    else:
+        text_error.draw()
+    win.flip()
+    time.sleep(1)
+
+# probe the pleasantness and tiredness of the participant
+myRatingScale = visual.RatingScale(win, low=0, high=100, marker="slider",
+    tickMarks=[0, 25, 50, 75, 100], stretch=1.5, tickHeight=1.5,  # singleClick=True,
+    labels=["0%", "25%", "50%", "75%", "100%"])
+myItem = visual.TextStim(win, text="", height=.08, units="norm")
+
+for quest in range(2):
+
+    ## remove any remaining ratings
+    myRatingScale.reset() 
+
+    if quest == 0:
+        myItem.text = "How pleasant was this experiment?"
+    else:
+        myItem.text = "How tired do you feel?"
+
+    ## show & update until a response has been made
+    while myRatingScale.noResponse:
+        myItem.draw()
+        myRatingScale.draw()
+        win.flip()
+        if event.getKeys("escape"):
+            core.quit()
+
+    print("Answer to question {0}: {1}%".format(str(quest), myRatingScale.getRating()))
+
+# display the average RT for one second
+meantime = numpy.mean(RT)
+text_feedback = visual.TextStim(win, text = "mean RT = {0:.1f} sec".format(meantime), pos = [0,0.5])
+text_feedback.draw()
+win.flip()
+time.sleep(1)
+
+# wrap it up
+win.close()
