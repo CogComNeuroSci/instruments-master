2eeb6864177d338fcda6c773d695a303cfb9e4fa
PieterVerbeke <pjverbek.verbeke@ugent.be>
2022-12-05 12:00:34 +0100

    adding files test 2

---
+++ 2022-2023/Test 2/Retake_test_2.py
@@ -0,0 +1,92 @@
+#Loading in modules
+from psychopy import visual, core
+import numpy as np
+
+#participant variables
+participant_number = 10
+participant_name = "Vincent Hoofs"
+participant_title = "Mr"
+
+#Design variables
+Nblocktrials = 8
+Nblocks = 2
+
+#Making window
+win = visual.Window([700, 700], color = (1,-1,1), units ='norm')
+
+#Making welcome message
+lastname = participant_name.split()[1]
+welcometext = "Welcome {0} {1}!".format(participant_title, lastname)
+welcome = visual.TextStim(win, text = welcometext, color = (-1,-1, -1))
+
+#Making instructions
+Instrtxt1 = "A colored shape will appear which increases in size.\n\n Your task is to indicate the shape of the target.\n\n If it's a circle press F,\n if it's a square press J."
+Instrtxt2 = "A colored shape will appear which increases in size.\n\n Your task is to indicate the color of the target.\n\n If it's red press F,\n if it's blue press J."
+Instructions = visual.TextStim(win, text = "", alignText = 'center', color = (-1,-1, -1))
+#Counterbalancing order
+if participant_number%2:
+    Instrtxt = [Instrtxt2, Instrtxt1]
+else:
+    Instrtxt = [Instrtxt1, Instrtxt2]  
+
+#Final message
+Thankmsg = visual.TextStim(win, text = "Thank you for participating", color = (-1,-1, -1))
+
+#4 stimuli
+Circle_red = visual.Circle(win, size = .1, fillColor = (1, -1, -1))
+Square_red = visual.rect.Rect(win, size = .1, fillColor = (1, -1, -1))
+
+Circle_blue = visual.Circle(win, size = .1, fillColor = (-1, -1, 1))
+Square_blue = visual.rect.Rect(win, size = .1, fillColor = (-1, -1, 1))
+
+#Feedback stimulus
+Feedback = visual.TextStim(win, text = "Too late!", color = (-1,-1, -1))
+
+#Gather all design variables in list
+sizes = 2*np.arange(.01, .16, .01)
+stimuli = [Circle_red, Square_red, Circle_blue, Square_blue]
+all_stimuli = np.repeat(stimuli, Nblocktrials/len(stimuli))
+
+#Display welcome message
+welcome.draw()
+win.flip()
+core.wait(5)
+
+#block loop
+for b in range(Nblocks):
+    #Display instructions depending on block
+    Instructions.text = Instrtxt[b%2]
+    Instructions.draw()
+    win.flip()
+    core.wait(5)
+    #trial loop
+    for t in range(Nblocktrials):
+        #stimulus loop
+        for s in sizes:
+            #Select stimuli from list (all_stimuli), adjust it's size and draw it on the screen
+            all_stimuli[t].size = s
+            all_stimuli[t].draw()
+            win.flip()
+            core.wait(.2)
+        #Blank screen
+        win.flip()
+        core.wait(.5)
+        
+        #Adjust feedback position depending on trial
+        if t%2:
+            Feedback.pos = (0,.5)
+        else:
+            Feedback.pos = (0,-.5)
+            
+        #Display feedback
+        Feedback.draw()
+        win.flip()
+        core.wait(1)
+
+#Display thank you message
+Thankmsg.draw()
+win.flip()
+core.wait(5)
+
+#Close window
+win.close()
