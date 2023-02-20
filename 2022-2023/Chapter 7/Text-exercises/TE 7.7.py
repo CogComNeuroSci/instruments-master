d176b26195dc53708b9e3fdacdc098139adb83c4
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2023-02-20 11:32:25 +0100

    Add files via upload

---
+++ 2022-2023/Chapter 7/Text-exercises/TE 7.7.py
@@ -0,0 +1,58 @@
+# basic file writing using psychopy
+# parity judgement task (even vs odd number discrimination)
+# using both ExperimentHandler and TrialHandler
+# attempts to write anyway in case of crash
+# note: to keep subject data anonymous, the subject's name is NOT used in the file name
+# only the subject's number is used
+# subject's name is asked only for greeting purposes
+# written by Tom Verguts, Feb 2019
+
+from psychopy import data, visual, event, core, gui, os
+
+info = {"Participant number": str(0), "Name": "Tom", "Session": str(0)}
+
+data_file = "experimental_data_"
+directory_to_write_to = os.getcwd() + "/data/"
+
+already_exists = True
+while already_exists:
+    myDlg = gui.DlgFromDict(info, title = "get subject info")
+    number = info["Participant number"]
+    filename = directory_to_write_to + data_file + number
+    if not os.path.isfile(filename + ".csv"):
+        already_exists = False
+
+thisExp = data.ExperimentHandler(dataFileName = filename)
+
+visual_targets = range(1,8)
+targets_responses = []
+for loop in visual_targets:
+    if loop%2 == 0:
+        correct_response = 'f' # even numbers, press f
+    else:
+        correct_response = 'j' # odd numbers, press j
+    targets_responses.append({'target':loop, 'correct_response':correct_response})
+
+trials = data.TrialHandler(targets_responses, nReps = 1, method = 'random')
+thisExp.addLoop(trials)
+
+win = visual.Window([400,400])
+experiment_timer = core.Clock()
+
+for trial in trials: # a TrialHandler object is iterable
+    the_text = visual.TextStim(win, text = trial['target'], color = 'white')
+    the_text.draw()
+    win.flip()
+    experiment_timer.reset()
+    response = event.waitKeys()
+    rt = experiment_timer.getTime()
+    accuracy = 0
+    if response[0] == trial['correct_response']:
+        accuracy = 1
+    trials.addData('response', response[0])
+    trials.addData('accuracy', accuracy)
+    trials.addData('RT',rt)
+    thisExp.nextEntry()
+
+win.close()
+core.quit()
