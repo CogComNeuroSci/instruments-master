039eef431b082a5fcf7a2e52062a33cd3a31f012
Tom Verguts <tom.verguts@ugent.be>
2023-07-08 10:09:24 +0200

    add ch 7 code

---
+++ In-text code/Chapter 7/Chapter_7_TrialHandler_recycle.py
@@ -0,0 +1,48 @@
+# basic file writing using psychopy
+# illustration that trialhandlers cannot be recycled
+# try putting trials = data.Trialhandler() outside the block loop
+# the program will crash the second time you want to go over the trials
+# because trialhandlers cannot be recycled.
+# in contrast, targets_responses is a list of dictionaries, so that CAN
+# be outside the block_loop
+# written by Tom Verguts, July 2023
+
+from psychopy import data, visual, event, core
+import os
+
+data_file = os.path.join(os.getcwd(), 'data', 'experimental_data')
+thisExp = data.ExperimentHandler(dataFileName=data_file)
+
+visual_targets = [digit for digit in range(1,5)]
+targets_responses = []
+for loop in visual_targets:
+    if loop%2 == 0:
+        correct_response = 'f' # even numbers, press f
+    else:
+        correct_response = 'j' # odd numbers, press j
+    targets_responses.append({'target':loop, 'correct_response':correct_response})
+
+
+win = visual.Window([400,400])
+experiment_timer = core.Clock()
+
+for block_loop in range(2):
+    trials = data.TrialHandler(targets_responses, nReps=1, method='random')
+    thisExp.addLoop(trials)
+    for trial in trials: # a TrialHandler object is iterable
+        the_text = visual.TextStim(win, text=trial['target'], color='white')
+        the_text.draw()
+        win.flip()
+        experiment_timer.reset()
+        response = event.waitKeys()
+        rt = experiment_timer.getTime()
+        accuracy = 0
+        if response[0]==trial['correct_response']:
+            accuracy = 1
+        trials.addData('response', response[0])
+        trials.addData('accuracy', accuracy)
+        trials.addData('RT',rt)
+        thisExp.nextEntry()
+
+win.close()
+core.quit()
