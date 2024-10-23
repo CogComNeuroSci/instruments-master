a71e6faae935306af05c09d5f6c83b1ce8289180
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2024-10-23 15:26:08 +0200

    update

---
+++ 2024-2025/Chapter 2/Text-exercises/TE 2.3 Small Screen.py
@@ -0,0 +1,13 @@
+# goal of this code: make a window, draw a window, and close it finally.
+
+from psychopy import visual
+# visual is a module from psychopy containing several visual methods
+
+import time 
+# time is a module from python containing several time-related methods
+# Hence, the window will be shown for 3 seconds exactly
+
+win = visual.Window([300,200])
+win.flip()
+time.sleep(3)
+win.close()
