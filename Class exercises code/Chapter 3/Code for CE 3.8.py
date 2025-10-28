d321053b3667319e3bb9cc6e04a49d1429b33a00
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2025-10-28 10:19:11 +0100

    update

--- Class exercises code/Chapter 3/Code for CE 3.8.py
+++ Class exercises code/Chapter 3/Code for CE 3.8.py
@@ -2,3 +2,7 @@
 # Max score on each course is 20.
 
 # initializing
+name        = "jasper francken"
+student_nr  = 2
+scores      = [12, 13, 9, 18]
+directory   = "/Users/tom/Documents/student_data/"  ## you'll want to insert a path for your own directory
