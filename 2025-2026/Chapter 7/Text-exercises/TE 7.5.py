2da361a0b1652a9c9cbde7132839c2ec864061df
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2026-02-12 11:10:46 +0100

    update

---
+++ 2025-2026/Chapter 7/Text-exercises/TE 7.5.py
@@ -0,0 +1,10 @@
+import numpy
+
+# Make a simple array
+x = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Export as a tab separated file
+numpy.savetxt('output_tab.txt', x, fmt='%d', delimiter = '\t') 
+
+# Export as a comma separated file
+numpy.savetxt('output_comma.txt', x, fmt='%d', delimiter = ',') 
